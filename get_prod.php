f49b548317a186ea4fbacf20da8e73c58eaa35ac
Terry <tgaitan@csumb.edu>
2018-03-27 06:57:38 +0000

    more chnages

--- get_prod.php
+++ get_prod.php
@@ -10,13 +10,7 @@ session_start();
 
 $albumn = $_POST ['Submit'];
 
-//query to get info on our product
-$sql = "SELECT album_name, songlist, artist_name, label_name, price FROM `Matt's_Inventory` natural join `Album` natural join 'Artist' natural join `Record_Label` WHERE album_name= : albumn";
 
-$stmt =$dbconn->prepare($sql);
-
-
-$stmt->execute ( array ( ':albumn'=> $albumn) );
 ?>
 
 <html>
@@ -26,26 +20,49 @@ $stmt->execute ( array ( ':albumn'=> $albumn) );
     <body>
     <div align="center">
         <table cellpadding= "5" width= "90%">
-            <tr>
-                <td><a href=</a></td>
-                <td><strong><?php echo $products_name; ?></strong><br>
-                <?php echo $products_prodesc; ?><br \>
-                <br>PRODUCT NUMBER:<?php echo $products_prodnum; ?> 
-                <br>PRODUCT PRICE: <?php echo $products_price; ?><br>
-                <form method="POST" action="modcart.php?action=add">
-                    Quantity: <input type="text" name="qty" size="2"><br>
-                    <input type="hidden" name= "products_prodnum" 
-                    value="<?php echo $products_prodnum; ?>"
-                    <input type="submit" name="SUBMIT" value="Add To Cart">
-                    </form>
-                    <form method="POST" action='cart.php'>
-                    <input type="submit" name="SUBMIT" value="View Cart">  
-                    </form>
-                </td>
-             </tr>
+           <?php 
+            
+            //query to get info on our product
+            $sql = " SELECT album_name,songlist,artist_name,label_name,price FROM `Matt's_Inventory` natural join `Album` natural join `Artist` natural join `Record_Label` WHERE album_name='$albumn' ";
+            $stmt =$dbconn->prepare($sql);
+            $stmt->execute();
+            echo "<td><img src='img/$albumn.jpg' alt='$albumn pic'/></td>";
+             if ($stmt->rowCount() > 0){
+                while($row=$stmt->fetch()){
+                    echo "<tr>";
+                    $abl_n = $row['album_name'];
+                    echo "<td><strong>Album Name: </strong>$abl_n</td><br>";
+                    $art_n = $row['artist_name'];
+                    echo "<td><strong>Album Name: </strong>$art_n</td><br>";
+                    $sglt = $row['songlist'];
+                    echo "<td><strong>Songlist</strong><br>$sglt</td><br>";
+                    $prc = $row['price'];
+                    echo "<td><strong>Price: $</strong>$prc</td>";
+                    /*<form method="POST" action="modcart.php?action=add">
+                        <input type="hidden" name= "products_prodnum" 
+                        value="<?php echo $products_prodnum; ?>"
+                        <input type="submit" name="SUBMIT" value="Add To Cart">
+                        </form>
+                        <form method="POST" action='cart.php'>
+                        <input type="submit" name="SUBMIT" value="View Cart">  
+                        </form>
+                    </td>
+                    */
+                    echo "</tr>";
+                    
+                }
+             }
+           
+           ?>
         </table>
         <hr width="200">
         <p><a href="ourstore.php">HOME</a></p>
     </div>
     </body>
 </html>
+
+
+
+
+
+
